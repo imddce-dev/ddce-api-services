377ec61a2974b16ec31e1bb8d941d9da77fcb9d7
Chonnavee555 <chonnavee.svv@Gmail.com>
2025-09-21 11:52:56 +0700

    docspage

---
+++ website/app/introduction/docs/nav.ts
@@ -0,0 +1,8 @@
+// app/introduction/docs/nav.ts
+export const navTree = [
+  { id: "objectives", title: "วัตถุประสงค์", href: "#objectives" },
+  { id: "getting-started", title: "เริ่มต้นใช้งาน", href: "#getting-started" },
+  { id: "standards", title: "มาตรฐาน", href: "#standards" },
+  { id: "token", title: "Token", href: "#token" },
+  { id: "gov", title: "มาตรฐานภาครัฐ", href: "#gov" },
+];
